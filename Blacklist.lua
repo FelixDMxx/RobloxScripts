7b1bcfe8484301e52bafb0fbd828f315f5e42a28
Felix <36384389+FelixDMxx@users.noreply.github.com>
2022-06-04 15:31:15 +0200

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,3 +1,11 @@
 local blacklist = {"RainbowJulieee","FelixDM"}
 
-if table.find(blacklist, game.Players.LocalPlayer.Name) then return false else return true end
+function check()
+	if table.find(blacklist, game:GetService("Players").LocalPlayer.Name) then 
+		return false 
+	else 
+		return true 
+	end
+end
+
+return check()
